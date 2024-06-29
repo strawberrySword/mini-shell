263ef0fcc3c3649c50cbbe486e4f8cb48860cfec
shaharg <shaharg@example.com>
2024-06-29 23:13:23 +0300

    feat: all basic functionality

--- myshell.c
+++ myshell.c
@@ -4,49 +4,70 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
+#include <fcntl.h>
 
 int process_arglist(int count, char** arglist){
     // simple case:
     int isWait = 0;
     int isPipe = 0;
+    int isInRedirection = 0;
+    int isAppenndRedirection = 0;
     int split;
     isWait = strcmp(arglist[count-1], "&");
+
     if(isWait == 0){
         arglist[count-1] = NULL;
     }
-
     for(int i =0; i<count; i++){
         if(strcmp(arglist[i], "|") == 0){
             isPipe = 1;
             split = i;
             arglist[i] = NULL;
+            break;
+        }
+        if(strcmp(arglist[i], "<") == 0){
+            isInRedirection = 1;
+            split = i;
+            arglist[i] = NULL;
+            break;
+        }
+        if(strcmp(arglist[i], ">>") == 0){
+            isAppenndRedirection = 1;
+            split = i;
+            arglist[i] = NULL;
+            break;
         }
     }
 
     if(isPipe){
         // create a pipe
         int pfds[2];
-        pipe(pfds);
+        if(pipe(pfds) == -1){
+            perror("pipe");
+            exit(1);
+        }
         pid_t pid = fork();    
         if(pid == 0){
             // change stdout to the pipe and close the pipe read
             close(pfds[0]);
             dup2(pfds[1], STDOUT_FILENO);
-            // close(pfds[1]);
+            close(pfds[1]);
             execvp(arglist[0], arglist);
             // this is failed FIX this
             return 1;
-        } 
+        }
         pid = fork();
         if(pid == 0){
-            printf("i am %s\n", arglist[split + 1]);
             // change stdin to the pipe and close the pipe write
             close(pfds[1]);
             dup2(pfds[0], STDIN_FILENO);
-            // close(pfds[0]);
-            execvp(arglist[split+1], arglist+ split + 1);
+            close(pfds[0]);
+            execvp(arglist[split + 1], arglist + split + 1);
+            // this is failed FIX this
             return 1;
         }
+        close(pfds[0]);
+        close(pfds[1]);
         int status = 0;
         if(isWait != 0){
             while(-1 != wait(&status));
@@ -55,10 +76,22 @@ int process_arglist(int count, char** arglist){
         
     }
 
+    
     pid_t pid = fork();
     
     if(pid == 0){
+        if(isInRedirection){
+            int file_desc = open(arglist[split+1], O_RDONLY);
+            dup2(file_desc, STDIN_FILENO);
+            close(file_desc);
+        }
+        if(isAppenndRedirection){
+            int file_desc = open(arglist[split+1], O_WRONLY | O_CREAT | O_APPEND);
+            dup2(file_desc, STDOUT_FILENO);
+            close(file_desc);
+        }
         execvp(arglist[0], arglist);
+        // this is failed FIX this
         return 1;
     } else {
         int status = 0;
